154f288a8a079aa81a2c42d75c31e1c005910cf8
unknown <ehero2001@gmail.com>
2023-09-07 17:20:57 +0700

    BTH

---
+++ Baithuchanh1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+    int sonnguyen;
+    float sothuc;
+    string chuoi;
+    char kytu;
+    bool dungsai;
+    sonnguyen = 6;
+    sothuc = 6.5f;
+    chuoi ="chuoi";
+    kytu ='d';
+    dungsai = true;
+    Console.WriteLine("Kieu so nguyen: {0}",sonnguyen);
+    Console.WriteLine("Kieu so nguyen: {0}",sothuc);
+    Console.WriteLine("Kieu so nguyen: {0}",chuoi);
+    Console.WriteLine("Kieu so nguyen: {0}",kytu);
+    Console.WriteLine("Kieu so nguyen: {0}",dungsai);
